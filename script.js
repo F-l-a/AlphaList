97982e48e8f3ec15b1136892706c8f5e94a1c986
Fla <flavio.bollea@gmail.com>
2026-01-23 05:40:05 +0100

    Bug Fixes
    - fix: routings

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('/AlphaList/service-worker.js')
             .then(reg => console.log('Service Worker Registered', reg))
             .catch(err => console.error('Service Worker Registration Failed:', err));
     }
